ff31d6297fdd2d7c8d89c5b0ad2822b99f8a7454
Juan Garcia <juanmgarcia098@gmail.com>
2023-02-27 11:23:11 -0400

    Testing

---
+++ controllers/profiles.js
@@ -0,0 +1,32 @@
+const { Profile, Vote } = require('../models')
+const cloudinary = require('cloudinary').v2
+
+async function index(req, res) {
+  try {
+    const profiles = await Profile.findAll({
+      include: [{ model: Vote, as: "votesReceived" }],
+    })
+    res.status(200).json(profiles)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ err: error })
+  }
+}
+
+async function addPhoto(req, res) {
+  try {
+    const imageFile = req.files.photo.path
+    const profile = await Profile.findByPk(req.params.id)
+    const image = await cloudinary.uploader.upload(
+      imageFile, {tags: `${req.user.email}`}
+    )
+    profile.photo = image.url
+    await profile.save()
+    res.status(201).json(profile.photo)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({err: error})
+  }
+}
+
+module.exports = { index, addPhoto }
---
+++ models/profile.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Profile extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Profile.belongsTo(models.User, { as: 'user', foreignKey: 'userId' })
+
+      Profile.hasMany(models.Vote, {
+        as: 'votesGiven',
+        foreignKey: 'voterId',
+      })
+  
+      Profile.hasMany(models.Vote, {
+        as: 'votesReceived',
+        foreignKey: 'profileId',
+      })
+  
+    }
+  }
+  
+  Profile.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    photo: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Profile',
+  });
+  return Profile;
+};
